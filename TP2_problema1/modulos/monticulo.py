9825c1de54e60c3016088426fd500f5a88550d1c
Ayrton Milessi <ayrton4210@gmail.com>
2023-10-02 10:30:10 -0300

    principio de monticulo

--- TP2_problema1/modulos/monticulo.py
+++ TP2_problema1/modulos/monticulo.py
@@ -1,1 +1,19 @@
-# -*- coding: utf-8 -*-
+class MonticuloBinario:
+    def __init__ (self):
+        self.lista_pacientes= [0]
+        self.tamanio= 0
+
+    def adelantar (self, orden):
+        while orden // 2 > 0:
+            if self.lista_pacientes[orden] < self.lista_pacientes [orden // 2]:
+                aux= self.lista_pacientes[orden // 2]
+                self.lista_pacientes[orden // 2]= self.lista_pacientes[orden]
+                self.lista_pacientes[orden]= aux
+            orden= orden // 2
+
+    def insertar (self, paciente):
+        self.lista_pacientes.append(paciente)
+        self.tamanio+= 1
+        self.adelantar(self.tamanio)
+
+    

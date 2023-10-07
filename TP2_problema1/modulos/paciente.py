5af28cb2828e701efd1e1c6920022c6fb8f79f14
Ayrton Milessi <ayrton4210@gmail.com>
2023-10-07 00:41:11 -0300

    paciente terminado

--- TP2_problema1/modulos/paciente.py
+++ TP2_problema1/modulos/paciente.py
@@ -6,7 +6,21 @@ nivel_riesgo= [1,2,3]
 
 class Paciente:
     def __init__ (self):
-        indice= len(nombres)
-        self.nombre= r.choice(nombres)
-        self.apellido= r.choice(apellidos)
-        self.riesgo= r.choice(nivel_riesgo)
+        self._nombre= r.choice(nombres)
+        self._apellido= r.choice(apellidos)
+        self._riesgo= r.choice(nivel_riesgo)
+
+    def get_nombre(self):
+        return self._nombre
+
+    def get_apellido(self):
+        return self._apellido
+
+    def get_riesgo(self):
+        return self._riesgo
+    
+    def __lt__(self, paciente_comparar):
+        return self._riesgo < paciente_comparar.get_riesgo()
+
+    def __gt__(self, paciente_comparar):
+        return self._riesgo > paciente_comparar.get_riesgo()

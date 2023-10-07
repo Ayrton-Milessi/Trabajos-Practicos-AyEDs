5e7f4eb5e3db80ebb2687a52896112d7e8785125
Ayrton Milessi <ayrton4210@gmail.com>
2023-10-07 01:31:27 -0300

    casi terminado

---
+++ TP2_problema1/modulos/sala.py
@@ -0,0 +1,17 @@
+from monticulo import MonticuloBinario
+
+class sala_de_emergencia:
+    def __init__(self):
+        self.monticulo = MonticuloBinario()
+
+    def ingresar_paciente(self, paciente):
+        self.monticulo.insertar(paciente)
+    
+    def atender_paciente(self):
+       return self.monticulo.eliminarMin()
+    
+    def total_pacientes(self):
+        return self.monticulo.tamanio
+    
+    def __iter__(self):
+        return iter(self.monticulo)

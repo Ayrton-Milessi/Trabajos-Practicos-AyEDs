8bd9a31a978dc2ad92883cf42a30d073d58a84b8
Ayrton-Milessi <141248568+Ayrton-Milessi@users.noreply.github.com>
2023-10-10 16:03:01 -0300

    otro avance avl

--- TP2_problema2/modulos/avl.py
+++ TP2_problema2/modulos/avl.py
@@ -11,3 +11,23 @@ class AVL:
 
    def __iter__(self):
        return self.raiz.__iter__()
+
+class Nodo_AVL:
+   def __init__(self, clave, valor, izquierdo= None, derecho= None, padre= None)
+     self.clave= clave
+     self.carga_util= valor
+     self.hijo_izquierdo= izquierdo
+     self.hijo_derecho= derecho
+     self.padre= padre
+
+   def tiene_hijo_izquierdo(self):
+     return self.hijo_izquierdo
+
+   def tiene_hijo_derecho(self):
+     return self.hijo_derecho
+
+   def es_hijo_izquierdo(self):
+     return self.padre and self.padre.hijo_izquierdo == self
+
+   def es_hijo_derecho(self):
+     return self.padre and self.padre.hijo_derecjo == self

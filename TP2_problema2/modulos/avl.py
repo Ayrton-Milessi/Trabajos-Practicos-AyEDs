4d093b326135939d8baa11b3e28032cf7edfc356
Ayrton-Milessi <141248568+Ayrton-Milessi@users.noreply.github.com>
2023-10-10 16:37:33 -0300

    prueba de agregar

--- TP2_problema2/modulos/avl.py
+++ TP2_problema2/modulos/avl.py
@@ -60,3 +60,32 @@ class Nodo_AVL:
       else:
          self.raiz= Nodo_AVL (clave, valor)
       self.tamanio += 1
+
+
+
+   def _agregar(self,clave,valor,nodoActual):
+    if clave < nodoActual.clave:
+        if nodoActual.tieneHijoIzquierdo():
+                self._agregar(clave,valor,nodoActual.hijoIzquierdo)
+        else:
+                nodoActual.hijoIzquierdo = NodoArbol(clave,valor,padre=nodoActual)
+                self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
+    else:
+        if nodoActual.tieneHijoDerecho():
+                self._agregar(clave,valor,nodoActual.hijoDerecho)
+        else:
+                nodoActual.hijoDerecho = NodoArbol(clave,valor,padre=nodoActual)
+                self.actualizarEquilibrio(nodoActual.hijoDerecho)
+
+def actualizarEquilibrio(self,nodo):
+    if nodo.factorEquilibrio > 1 or nodo.factorEquilibrio < -1:
+        self.reequilibrar(nodo)
+        return
+    if nodo.padre != None:
+        if nodo.esHijoIzquierdo():
+                nodo.padre.factorEquilibrio += 1
+        elif nodo.esHijoDerecho():
+                nodo.padre.factorEquilibrio -= 1
+
+        if nodo.padre.factorEquilibrio != 0:
+                self.actualizarEquilibrio(nodo.padre)

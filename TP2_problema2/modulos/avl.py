715343eb83a7be002be570a72b3f83fe1630a036
Ayrton Milessi <ayrton4210@gmail.com>
2023-10-11 16:03:38 -0300

    pequeño avance

--- TP2_problema2/modulos/avl.py
+++ TP2_problema2/modulos/avl.py
@@ -1,19 +1,5 @@
-class AVL:
-   def __init__(self):
-       self.raiz= None
-       self.tamanio= 0
-
-   def longitud(self):
-       return self.tamanio
-
-   def __len__(self):
-       return self.tamanio
-
-   def __iter__(self):
-       return self.raiz.__iter__()
-
 class Nodo_AVL:
-   def __init__(self, clave, valor, izquierdo= None, derecho= None, padre= None)
+   def __init__(self, clave, valor, izquierdo= None, derecho= None, padre= None):
      self.clave= clave
      self.carga_util= valor
      self.hijo_izquierdo= izquierdo
@@ -30,7 +16,7 @@ class Nodo_AVL:
      return self.padre and self.padre.hijo_izquierdo == self
 
    def es_hijo_derecho(self):
-     return self.padre and self.padre.hijo_derecjo == self
+     return self.padre and self.padre.hijo_derecho == self
 
    def es_raiz(self):
      return not self.padre
@@ -53,39 +39,83 @@ class Nodo_AVL:
           self.hijo_izquierdo.padre = self
       if self.tiene_hijo_derecho():
           self.hijo_derecho.padre = self
+  
+   def empalmar(self):
+      if self.es_hoja():
+          if self.es_hijo_izquierdo():
+              self.padre.hijo_izquierdo= None
+          else:
+              self.padre.hijo_derecho= None
+      elif self.tiene_un_hijo():
+          if self.tiene_hijo_izquierdo():
+              if self.es_hijo_izquierdo():
+                  self.padre.hijo_izquierdo= self.hijo_izquierdo
+              else:
+                  self.padre.hijo_derecho= self.hijo_izquierdo
+              self.hijo_izquierdo.padre= self.padre
+          else:
+              if self.es_hijo_izquierdo():
+                  self.padre.hijo_izquierdo= self.hijo_derecho
+              else:
+                  self.padre.hijo_derecho= self.hijo_derecho
+              self.hijo_derecho.padre= self.padre
 
-   def agregar(self,clave, valor):
-      if self.raiz:
-         self._agregar(clave, valor, self.raiz)
+   def encontrar_sucesor(self):
+      sucesor= None
+      if self.tiene_hijo_derecho():
+          sucesor= self.hijo_derecho.encontrar_minimo()
       else:
-         self.raiz= Nodo_AVL (clave, valor)
-      self.tamanio += 1
+          if self.padre:
+            if self.es_hijo_izquierdo():
+              sucesor= self.padre
+            else:
+              self.padre.hijo_derecho= None
+              sucesor= self.padre.encontrar_sucesor()
+              self.padre.hijo_derecho = self
+      return sucesor
 
+   def encontrar_minimo(self):
+      actual= self
+      while actual.tiene_hijo_izquierdo():
+          actual= actual.hijo_izquierdo
+      return actual
 
+class AVL:
+   def __init__(self):
+       self.raiz= None
+       self.tamanio= 0
 
-   def _agregar(self,clave,valor,nodoActual):
-    if clave < nodoActual.clave:
-        if nodoActual.tieneHijoIzquierdo():
-                self._agregar(clave,valor,nodoActual.hijoIzquierdo)
+   def longitud(self):
+        return self.tamanio
+
+   def __len__(self):
+       return self.tamanio
+
+   def __iter__(self):
+       return self.raiz.__iter__()
+
+   def agregar(self, clave, valor):
+        if self.raiz:
+            self._agregar(clave, valor, self.raiz)
         else:
-                nodoActual.hijoIzquierdo = NodoArbol(clave,valor,padre=nodoActual)
-                self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
-    else:
-        if nodoActual.tieneHijoDerecho():
-                self._agregar(clave,valor,nodoActual.hijoDerecho)
-        else:
-                nodoActual.hijoDerecho = NodoArbol(clave,valor,padre=nodoActual)
-                self.actualizarEquilibrio(nodoActual.hijoDerecho)
+            self.raiz= Nodo_AVL(clave, valor)
+        self.tamanIo += 1
 
-def actualizarEquilibrio(self,nodo):
-    if nodo.factorEquilibrio > 1 or nodo.factorEquilibrio < -1:
-        self.reequilibrar(nodo)
-        return
-    if nodo.padre != None:
-        if nodo.esHijoIzquierdo():
-                nodo.padre.factorEquilibrio += 1
-        elif nodo.esHijoDerecho():
-                nodo.padre.factorEquilibrio -= 1
+   def _agregar(self, clave, valor, nodoActual):
+      pass
+   
+   def __setitem__(self,c,v):
+       self.agregar(c,v)
+    
+   def obtener(self, clave):
+       if self.raiz:
+           resultado= self._obtener(clave, self.raiz)
+           if resultado:
+                  return resultado.carga_util
+           else:
+                  return None
+       else:
+           return None
 
-        if nodo.padre.factorEquilibrio != 0:
-                self.actualizarEquilibrio(nodo.padre)
+   def _obtener(self,clave,nodoActual):
+       pass

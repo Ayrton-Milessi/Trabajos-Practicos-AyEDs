caf42b3ae291ea6c82b01113e6d109ce9aa55c78
Elias-C0 <elias_prueba@hotmail.com>
2023-10-20 00:16:47 -0300

    obtener precio minimo funcional

--- TP2_problema3/Aplicacion/main.py
+++ TP2_problema3/Aplicacion/main.py
@@ -24,10 +24,17 @@ with open("rutas.txt", "r") as arch:
             grafo.agregarArista(nombre1,nombre2, peso_max, costo)
 
 
-#para imprimir el grafo por consola
+#Este fragemento sirve para imprimir el grafo en consola
 for vertice in grafo.obtenerVertices():
     print(f"Vertice: {vertice}")
     for vecino in grafo.listaVertices[vertice].obtenerConexiones():
         capacidad, costo = grafo.listaVertices[vertice].obtenerPonderacion(vecino)
         print(f"Camino hacia {vecino.obtenerId()} - Peso máximo: {capacidad} - Precio: {costo}")
     print("\n")
+
+
+inicio= "Rosario" #elegimos la ciudad de origen
+destino= "Rufino" #elegimos la ciudad de destino final
+precio= grafo.obtener_precio_minimo(grafo ,inicio ,destino)
+
+print(f"El precio mas bajo para viajar desde {inicio} a {destino} es de ${int(precio)*1000}")

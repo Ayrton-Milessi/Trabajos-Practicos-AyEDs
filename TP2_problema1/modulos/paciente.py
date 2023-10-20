b1be1b4cee23289c6716230e004e3a054a1b81eb
Ayrton Milessi <ayrton4210@gmail.com>
2023-10-20 00:57:17 -0300

    comparación de tiempo de llegada de los pacientes

--- TP2_problema1/modulos/paciente.py
+++ TP2_problema1/modulos/paciente.py
@@ -1,4 +1,5 @@
 from random import randint, choices
+import datetime
 
 nombres= ["Sofía", "Jorge", "Matías", "Alejandro", "Pedro", "Juan", "Valeria", "Agustina"]
 apellidos= ["Gonzalez", "Perez", "Rodriguez", "Messi", "Palacios", "Alvarez", "Martinez", "Sivori"]
@@ -14,6 +15,7 @@ class Paciente:
         self.__apellido = apellidos[randint(0, n-1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
         self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
+        self.tiempo_llegada = datetime.datetime.now()
 
     def get_nombre(self):
         return self.__nombre
@@ -34,7 +36,11 @@ class Paciente:
         return cad
     
     def __lt__(self, paciente_comparar):
+        if self.__riesgo == paciente_comparar.__riesgo:
+            return self.tiempo_llegada < paciente_comparar.tiempo_llegada
         return self.__riesgo < paciente_comparar.__riesgo
 
     def __gt__(self, paciente_comparar):
+        if self.__riesgo == paciente_comparar.__riesgo:
+            return self.tiempo_llegada > paciente_comparar.tiempo_llegada
         return self.__riesgo > paciente_comparar.__riesgo
